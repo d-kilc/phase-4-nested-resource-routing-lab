5299962f7909417d9f4f0f5f4e03879eb750c27b
Daniel <dkilcullen25@gmail.com>
2022-02-24 16:39:08 -0600

    commit tree 53874112d0cdbedaed0aa62a50679c49da600da4

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,38 @@
-class ItemsController < ApplicationController
+class ItemsController < ApplicationController 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :render_record_invalid
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find params[:user_id]
+      items = Item.where user: user
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
 
+  def show
+    item = Item.find params[:id]
+    render json: item, status: 200
+  end
+
+  def create
+    item = Item.create! items_params
+    render json: item, status: 201
+  end
+
+  private
+
+  def render_record_not_found
+    render status: :not_found
+  end
+
+  def render_record_invalid
+    render status: :unprocessable_entity
+  end
+
+  def items_params
+    params.permit(:name,:description,:price,:user_id)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,19 @@
 class UsersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
 
   def show
-    user = User.find_by(id: params[:id])
+    user = User.find params[:id]
     render json: user, include: :items
+  end
+
+  def index
+    render json: User.all
+  end
+
+  private
+
+  def render_record_not_found
+    render status: 404
   end
 
 end
